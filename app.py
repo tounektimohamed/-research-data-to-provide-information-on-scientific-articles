c23cb70a20869019f368430030877858534dcd0c
tounektimohamed <tounekti17@gmail.com>
2024-10-22 20:38:32 +0100

    first commit

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def login():
     google_auth_url = (
         f"https://accounts.google.com/o/oauth2/v2/auth?"
         f"client_id={GOOGLE_CLIENT_ID}&"
-        f"redirect_uri=http://localhost:5000/callback&"
+        f"redirect_uri=https://research-data-to-provide-information-on.onrender.com/callback"
         f"response_type=code&"
         f"scope=email profile"
     )
